ed01981f47a54a998f773a2925b10c164f803c26
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2025-12-23 10:21:49 +0500

    User Quiver Quantitative saved changes to main.py at 12/23/2025, 05:21:49 UTC time.

--- f8f7fe3a-914e-4394-93cc-9361f0b9863c/main.py
+++ f8f7fe3a-914e-4394-93cc-9361f0b9863c/main.py
@@ -1,1 +1,25 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation, backtest
+from surmount.data import WSBTop10
+from surmount.logging import log
+class TradingStrategy(Strategy):
+    def __init__(self):
+        self.data_list = [WSBTop10()]
+        self.tickers = []
+    @property
+    def interval(self):
+        return "1day"
+    @property
+    def assets(self):
+        return self.tickers
+    @property
+    def data(self):
+        return self.data_list
+    def run(self, data):
+        wsb_top_10_holdings = data[("wsb_top_10",)]
+        allocations = {"AAPL": 1}
+        if wsb_top_10_holdings:
+            alloc_dict = wsb_top_10_holdings[-1]['allocations']
+            #log(f"Trading: {wsb_top_10_holdings[-1]['allocations']}")
+            allocations = alloc_dict
+        #log(f"allocations:{allocations}")
+        return TargetAllocation(allocations)
