32379d35fd6a46e0220c71c400dacff6d929f913
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2025-12-24 09:48:10 +0500

    User Quiver Quantitative saved changes to main.py at 12/24/2025, 04:48:09 UTC time.

--- 3e24bffa-2bdc-479e-a23c-f2c1e35a32eb/main.py
+++ 3e24bffa-2bdc-479e-a23c-f2c1e35a32eb/main.py
@@ -1,1 +1,25 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation, backtest
+from surmount.data import InsiderPurchases
+from surmount.logging import log
+class TradingStrategy(Strategy):
+    def __init__(self):
+        self.data_list = [InsiderPurchases()]
+        self.tickers = []
+    @property
+    def interval(self):
+        return "1day"
+    @property
+    def assets(self):
+        return self.tickers
+    @property
+    def data(self):
+        return self.data_list
+    def run(self, data):
+        insider_purchases_holdings = data[("insider_purchases",)]
+        allocations = {"AAPL": 1}
+        if insider_purchases_holdings:
+            alloc_dict = insider_purchases_holdings[-1]['allocations']
+            log(f"Trading: {insider_purchases_holdings[-1]['allocations']}")
+            allocations = alloc_dict
+        log(f"allocations:{allocations}")
+        return TargetAllocation(allocations)
