9673eae96879990a617f1e6fbe18e33ca84f266a
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2026-01-09 22:41:08 +0500

    User Quiver Quantitative saved changes to main.py at 01/09/2026, 17:41:07 UTC time.

--- 4810a3cf-9b6f-456c-9b81-fe8f99c656b4/main.py
+++ 4810a3cf-9b6f-456c-9b81-fe8f99c656b4/main.py
@@ -1,1 +1,30 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation, backtest
+from surmount.data import InverseCNBC
+from surmount.logging import log
+
+class TradingStrategy(Strategy):
+    def __init__(self):
+        self.data_list = [InverseCNBC()]
+        self.tickers = []
+
+    @property
+    def interval(self):
+        return "1day"
+
+    @property
+    def assets(self):
+        return self.tickers
+
+    @property
+    def data(self):
+        return self.data_list
+
+    def run(self, data):
+        inverse_cnbc_holdings = data[("inverse_cnbc",)]
+        allocations = {"AAPL": 1}
+        if inverse_cnbc_holdings:
+            alloc_dict = inverse_cnbc_holdings[-1]['allocations']
+            #log(f"Trading: {inverse_cnbc_holdings[-1]['allocations']}")
+            allocations = alloc_dict
+        #log(f"allocations:{allocations}")
+        return TargetAllocation(allocations)
