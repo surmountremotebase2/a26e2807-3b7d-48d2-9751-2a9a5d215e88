e9c4e22786645bff20b6bcaed14a5ccdf9ef42be
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2026-01-09 22:23:01 +0500

    User Quiver Quantitative saved changes to main.py at 01/09/2026, 17:23:00 UTC time.

--- 73fc1557-7e37-4e71-bda4-1c778effbe4c/main.py
+++ 73fc1557-7e37-4e71-bda4-1c778effbe4c/main.py
@@ -1,1 +1,30 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation, backtest
+from surmount.data import HouseEnergyAndCommerceCommittee
+from surmount.logging import log
+
+class TradingStrategy(Strategy):
+    def __init__(self):
+        self.data_list = [HouseEnergyAndCommerceCommittee()]
+        self.tickers = []
+
+    @property
+    def interval(self):
+        return "1day"
+
+    @property
+    def assets(self):
+        return self.tickers
+
+    @property
+    def data(self):
+        return self.data_list
+
+    def run(self, data):
+        house_energy_and_commerce_committee_holdings = data[("house_energy_and_commerce_committee",)]
+        allocations = {"AAPL": 1}
+        if house_energy_and_commerce_committee_holdings:
+            alloc_dict = house_energy_and_commerce_committee_holdings[-1]['allocations']
+            log(f"Trading: {house_energy_and_commerce_committee_holdings[-1]['allocations']}")
+            allocations = alloc_dict
+        log(f"allocations:{allocations}")
+        return TargetAllocation(allocations)
