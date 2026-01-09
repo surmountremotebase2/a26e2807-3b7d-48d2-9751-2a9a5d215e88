44bd52a470d0acf07b97522c0557222d2ce6c2a2
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2026-01-09 22:07:23 +0500

    User Quiver Quantitative saved changes to main.py at 01/09/2026, 17:07:22 UTC time.

--- 588ed1d0-693e-4314-9d21-791b9c371754/main.py
+++ 588ed1d0-693e-4314-9d21-791b9c371754/main.py
@@ -1,1 +1,25 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation, backtest
+from surmount.data import CongressSells
+from surmount.logging import log
+class TradingStrategy(Strategy):
+    def __init__(self):
+        self.data_list = [CongressSells()]
+        self.tickers = []
+    @property
+    def interval(self):
+        return "1day"
+    @property
+    def assets(self):
+        return self.tickers
+    @property
+    def data(self):
+        return self.data_list
+    def run(self, data):
+        congress_sells_holdings = data[("congress_sells",)]
+        allocations = {"AAPL": 1}
+        if congress_sells_holdings:
+            alloc_dict = congress_sells_holdings[-1]['allocations']
+            log(f"Trading: {congress_sells_holdings[-1]['allocations']}")
+            allocations = alloc_dict
+        log(f"allocations:{allocations}")
+        return TargetAllocation(allocations)
