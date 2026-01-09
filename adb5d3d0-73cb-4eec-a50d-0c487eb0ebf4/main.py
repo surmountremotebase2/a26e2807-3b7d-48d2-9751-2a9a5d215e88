b561d0d77dadeb2e3fb3c16e285e54b5231c0a72
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2026-01-09 23:24:04 +0500

    User Quiver Quantitative saved changes to main.py at 01/09/2026, 18:24:04 UTC time.

--- adb5d3d0-73cb-4eec-a50d-0c487eb0ebf4/main.py
+++ adb5d3d0-73cb-4eec-a50d-0c487eb0ebf4/main.py
@@ -1,1 +1,30 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation, backtest
+from surmount.data import TopGovernmentContractReceivers
+from surmount.logging import log
+
+class TradingStrategy(Strategy):
+    def __init__(self):
+        self.data_list = [TopGovernmentContractReceivers()]
+        self.tickers = []
+
+    @property
+    def interval(self):
+        return "1day"
+
+    @property
+    def assets(self):
+        return self.tickers
+
+    @property
+    def data(self):
+        return self.data_list
+
+    def run(self, data):
+        top_government_contract_receivers_holdings = data[("top_government_contract_receivers",)]
+        allocations = {"AAPL": 1}
+        if top_government_contract_receivers_holdings:
+            alloc_dict = top_government_contract_receivers_holdings[-1]['allocations']
+            #log(f"Trading: {top_government_contract_receivers_holdings[-1]['allocations']}")
+            allocations = alloc_dict
+        #log(f"allocations:{allocations}")
+        return TargetAllocation(allocations)
