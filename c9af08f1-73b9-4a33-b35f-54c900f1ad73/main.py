a11aeb94b6e97745adb87976d0c63f64135c09e5
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2026-01-09 22:43:15 +0500

    User Quiver Quantitative saved changes to main.py at 01/09/2026, 17:43:14 UTC time.

--- c9af08f1-73b9-4a33-b35f-54c900f1ad73/main.py
+++ c9af08f1-73b9-4a33-b35f-54c900f1ad73/main.py
@@ -1,1 +1,30 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation, backtest
+from surmount.data import InverseCramer
+from surmount.logging import log
+
+class TradingStrategy(Strategy):
+    def __init__(self):
+        self.data_list = [InverseCramer()]
+        self.tickers = []
+
+    @property
+    def interval(self):
+        return "1day"
+
+    @property
+    def assets(self):
+        return self.tickers
+
+    @property
+    def data(self):
+        return self.data_list
+
+    def run(self, data):
+        inverse_cramer_holdings = data[("inverse_cramer",)]
+        allocations = {"AAPL": 1}
+        if inverse_cramer_holdings:
+            alloc_dict = inverse_cramer_holdings[-1]['allocations']
+            #log(f"Trading: {inverse_cramer_holdings[-1]['allocations']}")
+            allocations = alloc_dict
+        #log(f"allocations:{allocations}")
+        return TargetAllocation(allocations)
