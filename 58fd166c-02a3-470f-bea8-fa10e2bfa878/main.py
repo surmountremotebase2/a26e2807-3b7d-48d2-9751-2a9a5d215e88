97847397ed1c3adb7617d178f05771623529e1e9
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2026-01-09 23:33:28 +0500

    User Quiver Quantitative saved changes to main.py at 01/09/2026, 18:33:27 UTC time.

--- 58fd166c-02a3-470f-bea8-fa10e2bfa878/main.py
+++ 58fd166c-02a3-470f-bea8-fa10e2bfa878/main.py
@@ -1,1 +1,30 @@
-#Type code here
+from surmount.base_class import Strategy, TargetAllocation, backtest
+from surmount.data import WikipediaMostViewedMonth
+from surmount.logging import log
+
+class TradingStrategy(Strategy):
+    def __init__(self):
+        self.data_list = [WikipediaMostViewedMonth()]
+        self.tickers = []
+
+    @property
+    def interval(self):
+        return "1day"
+
+    @property
+    def assets(self):
+        return self.tickers
+
+    @property
+    def data(self):
+        return self.data_list
+
+    def run(self, data):
+        wikipedia_most_viewed_month_holdings = data[("wikipedia_most_viewed_month",)]
+        allocations = {"AAPL": 1}
+        if wikipedia_most_viewed_month_holdings:
+            alloc_dict = wikipedia_most_viewed_month_holdings[-1]['allocations']
+            #log(f"Trading: {wikipedia_most_viewed_month_holdings[-1]['allocations']}")
+            allocations = alloc_dict
+        #log(f"allocations:{allocations}")
+        return TargetAllocation(allocations)
